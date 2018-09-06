b1a1dea3fe90f61af7363ee57ce4466208aaf697
洪遥 <41867103+hytc5555@users.noreply.github.com>
2018-09-06 18:13:44 +0800

    Initial commit

---
+++ src/main/java/com/collection/hy/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.collection.hy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
